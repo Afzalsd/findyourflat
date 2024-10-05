9727f37d77a8d6853ff2a40d267178ebc57b623e
Sree Satya Harsha <harsha.pss2005@gmail.com>
2024-10-05 17:15:43 +0530

    add login and signup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import {Routes, Route} from 'react-router-dom'
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 import PropertyDetails from './pages/PropertyDetails';
 
 const App = () => {
@@ -12,6 +14,8 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/property/:id' element={<PropertyDetails />} />
+      <Route path='/login' element={<Login />}/>
+      <Route path='/register' element={<Register />}/>
     </Routes>
     <Footer />
   </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -11,8 +11,8 @@ const Header = () => {
 
       {/* navigation */}
       <div className=' flex items-center gap-6 '>
-        <Link to='' className='hover:text-violet-900 transition'>Log in</Link>
-        <Link to='' className='bg-violet-700 hover:bg-white hover:text-violet-700 text-white px-4 py-3 rounded-lg transition'>Sign up</Link>
+        <Link to='/login' className='hover:text-violet-900 transition'>Log in</Link>
+        <Link to='/register' className='bg-violet-700 hover:bg-white hover:text-violet-700 text-white px-4 py-3 rounded-lg transition'>Sign up</Link>
       </div>
     </div>
   </header>
